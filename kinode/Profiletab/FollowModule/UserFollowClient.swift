4886e6c5e9e53b2405f0167d691569275ba3dd37
f0rc <brxcomail@gmail.com>
2023-11-28 14:36:25 -0600

    init follow

---
+++ kinode/Profiletab/FollowModule/UserFollowClient.swift
@@ -0,0 +1,39 @@
+//
+//  UserFollowClient.swift
+//  kinode
+//
+//  Created by  on 11/27/23.
+//
+
+import Foundation
+
+
+struct userFollowServerInput: Codable {
+    let sessionToken: String
+    let userToFollowId: String
+}
+
+struct UserFollowServerResponse: Codable {
+    let status: String
+    let message: String?
+}
+
+enum UserFollowApiError: Error {
+    case failedJsonEncode
+    case statusNotSuccess
+}
+
+func followUser(reqInput: userFollowServerInput) async throws -> Bool {
+    let httpClient = StoreHTTPClient()
+    
+    let jsonDataInput = try JSONEncoder().encode(reqInput)
+    
+    let response: UserFollowServerResponse = try await httpClient.load(Resource(url: URL.followUser, method: .post(jsonDataInput)))
+    
+    if response.status != "success" {
+        print(response.message)
+        throw UserFollowApiError.statusNotSuccess
+    }
+    
+    return true
+}
